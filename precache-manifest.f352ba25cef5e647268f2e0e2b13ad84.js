df344005028ee7514fba8898273aa46d4a2014bc
Jonathan Wang <youchun2@illinois.edu>
2022-09-02 02:29:27 -0500

    Updates

---
+++ precache-manifest.f352ba25cef5e647268f2e0e2b13ad84.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d57d4ff1465dd3d8a18c",
+    "url": "/static/css/main.4260618e.chunk.css"
+  },
+  {
+    "revision": "d57d4ff1465dd3d8a18c",
+    "url": "/static/js/main.ce3710d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "042faf175836111ef0f6",
+    "url": "/static/css/2.28dd000b.chunk.css"
+  },
+  {
+    "revision": "042faf175836111ef0f6",
+    "url": "/static/js/2.8c0b43a6.chunk.js"
+  },
+  {
+    "revision": "371bdaed68c12ed1f49c31f8da98ec4b",
+    "url": "/static/media/self.371bdaed.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/static/media/portfolio1.32a90284.jpg"
+  },
+  {
+    "revision": "fa26fb360521ccd68aff1a2e850f7c29",
+    "url": "/static/media/portfolio2.fa26fb36.jpeg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "669fc84a86a4daa8d43727a446f96e4b",
+    "url": "/static/media/portfolio4.669fc84a.jpeg"
+  },
+  {
+    "revision": "9eecc0afc558113d8dda8a0337204244",
+    "url": "/static/media/portfolio5.9eecc0af.png"
+  },
+  {
+    "revision": "027d6ad108a8f9656d031a0c1dc247c5",
+    "url": "/static/media/portfolio6.027d6ad1.jpeg"
+  },
+  {
+    "revision": "4180b5c740083220a459f693da5bf9a2",
+    "url": "/index.html"
+  }
+];
